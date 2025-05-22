e1d69d19d2bfddd13e11da2502960153a3f7879c
jehoshua-shey <jehoshuachukwuemeka@gmail.com>
2025-05-22 14:03:55 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,12 @@ export default function Home() {
 export const metadata = {
   other: {
     'fc:frame': 'next',
-    'fc:frame:image': 'https://yourdomain.com/og-image.png',
+    'fc:frame:image': 'https://farcaster-mon-faucet.vercel.app/image.png',
     'fc:frame:button:1': 'Claim MON',
     'fc:frame:button:1:action': 'launch_frame',
-    'fc:frame:button:1:target': 'https://yourdomain.com/',
+    'fc:frame:button:1:target': 'https://farcaster-mon-faucet.vercel.app/',
+    'fc:frame:button:1:name': 'Monverge Free MON Faucet', // Optional: App name
+    'fc:frame:button:1:splashImageUrl': 'https://farcaster-mon-faucet.vercel.app/splash.png', // Optional: Splash image
+    'fc:frame:button:1:splashBackgroundColor': '#ffffff', // Optional: Splash background color
   },
 };
