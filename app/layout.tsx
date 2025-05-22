5a5efce5559bde745ece8326bbdfd656f1a50ec2
jehoshua-shey <jehoshuachukwuemeka@gmail.com>
2025-05-22 16:28:06 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+import { FrameProvider } from '@/components/farcaster-provider';
 
 export const metadata: Metadata = {
     title: 'Claim free mon from Monverge',
@@ -13,7 +14,11 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <FrameProvider>
+                    {children}
+                </FrameProvider>
+            </body>
         </html>
     );
 }
