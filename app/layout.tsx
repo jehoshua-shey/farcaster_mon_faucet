95ca7be4be438ac73ea35895f51ca19a9ae9a0d1
jehoshua-shey <jehoshuachukwuemeka@gmail.com>
2025-05-22 14:35:08 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 
-import { sdk } from '@farcaster/frame-sdk'
- 
-await sdk.actions.ready();
-
 export const metadata: Metadata = {
     title: 'Claim free mon from Monverge',
     description: 'Claim 0.1 MON on Monad Testnet',
